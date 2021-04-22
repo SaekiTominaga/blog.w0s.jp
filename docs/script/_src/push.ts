d00f5a21c49a83b27c412d5a63cfa5bb2426fc50
SaekiTominaga <kcsu3470+github@gmail.com>
2021-04-22 15:21:06 +0900

    プッシュ通知関係のスクリプトは使っていないので削除

--- docs/script/_src/push.ts
+++
@@ -1,6 +0,0 @@
-import PushRegist from './unique/PushRegist';
-
-/**
- * プッシュ通知登録ページ
- */
-new PushRegist('/push/regist', 'BKgMqiChJICPjX_KSGvklJ9G0jMFBKW0b5fsgU8-2moxDO0c11NColsIciULJTAOwsDJFxfaWtNKKIc9ng_ghbw').init();
