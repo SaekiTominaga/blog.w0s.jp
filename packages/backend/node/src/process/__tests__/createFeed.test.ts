6a3946ea96ec54f02b6f437331183a8361aa704a
SaekiTominaga <kcsu3470+github@gmail.com>
2024-02-12 23:04:31 +0900

    テスト作成

---
+++ packages/backend/node/src/process/__tests__/createFeed.test.ts
@@ -0,0 +1,13 @@
+import fs from 'node:fs';
+import CreateFeed from '../CreateFeed.js';
+import type { NoName as Configure } from '../../../../configure/type/common.js';
+
+const config = JSON.parse(await fs.promises.readFile('configure/common.json', 'utf8')) as Configure;
+
+const result = await new CreateFeed().execute({
+	dbFilePath: config.sqlite.db.blog,
+	views: config.views,
+	prettierConfig: config.prettier.config,
+	root: config.static.root,
+});
+console.debug(result);
