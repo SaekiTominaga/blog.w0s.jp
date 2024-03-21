919ddbbbde2c3f98a5db9b96ba711f361dac7b97
SaekiTominaga <kcsu3470+github@gmail.com>
2024-03-21 18:17:11 +0900

    微調整（アルゴリズムに変化なし）

--- packages/backend/node/src/util/Compress.ts
+++ packages/backend/node/src/util/Compress.ts
@@ -1,22 +1,22 @@
-import zlib from 'zlib';
+import zlib from 'node:zlib';
 
 /**
  * Compression functionality
  */
 export default class Compress {
 	/**
-	 * Compress a UTF-8 text with BrotliCompress.
+	 * Compress a UTF-8 text with Brotli
 	 *
-	 * @param string - Text data to be compressed
+	 * @param text - Text data to be compressed
 	 *
 	 * @returns Compressed data
 	 */
-	static brotliText(string: string): Buffer {
-		return zlib.brotliCompressSync(string, {
+	static brotliText(text: string): Buffer {
+		return zlib.brotliCompressSync(text, {
 			params: {
 				[zlib.constants.BROTLI_PARAM_MODE]: zlib.constants.BROTLI_MODE_TEXT,
 				[zlib.constants.BROTLI_PARAM_QUALITY]: zlib.constants.BROTLI_MAX_QUALITY,
-				[zlib.constants.BROTLI_PARAM_SIZE_HINT]: string.length,
+				[zlib.constants.BROTLI_PARAM_SIZE_HINT]: text.length,
 			}, // https://nodejs.org/api/zlib.html#compressor-options
 		});
 	}
