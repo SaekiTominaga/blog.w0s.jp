87cbe01c0eb1eff2be3db56355c2c33b1083ee21
SaekiTominaga <kcsu3470+github@gmail.com>
2025-01-10 17:23:26 +0900

    Twitter 埋め込みコードの削除忘れ

--- frontend/script/trusted-types.ts
+++ frontend/script/trusted-types.ts
@@ -15,7 +15,7 @@ if (window.trustedTypes !== undefined) {
 		},
 		createScriptURL: (inputUrl: string): string => {
 			const ALLOW_URLS: string[] = ['https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'];
-			const ALLOW_ORIGINS: string[] = ['https://platform.twitter.com'];
+			const ALLOW_ORIGINS: string[] = [];
 			const ALLOW_REGEXPS: RegExp[] = [];
 
 			if (!ALLOW_URLS.includes(inputUrl) && !ALLOW_ORIGINS.includes(new URL(inputUrl).origin) && !ALLOW_REGEXPS.some((allow) => allow.test(inputUrl))) {
