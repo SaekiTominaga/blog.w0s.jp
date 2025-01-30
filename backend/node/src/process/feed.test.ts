7446e378cc1804dc66b25662e1899b695704617f
SaekiTominaga <kcsu3470+github@gmail.com>
2025-01-30 19:26:45 +0900

    テスト修正

--- backend/node/src/process/feed.test.ts
+++ backend/node/src/process/feed.test.ts
@@ -5,5 +5,7 @@ import create from './feed.js';
 await test('create', async () => {
 	const result = await create();
 
-	assert.deepEqual(result.files, ['../frontend/public/feed.atom', '../frontend/public/feed.atom.br']);
+	assert.equal(result.files.length, 2);
+	assert.equal(result.files.includes('../frontend/public/feed.atom'), true);
+	assert.equal(result.files.includes('../frontend/public/feed.atom.br'), true);
 });
--- backend/node/src/process/newlyJson.test.ts
+++ backend/node/src/process/newlyJson.test.ts
@@ -5,14 +5,13 @@ import create from './newlyJson.js';
 await test('create', async () => {
 	const result = await create();
 
-	assert.deepEqual(result.files, [
-		'../frontend/public/json/newly.json',
-		'../frontend/public/json/newly.json.br',
-		'../frontend/public/json/newly_web.json',
-		'../frontend/public/json/newly_web.json.br',
-		'../frontend/public/json/newly_kumeta.json',
-		'../frontend/public/json/newly_kumeta.json.br',
-		'../frontend/public/json/newly_railway.json',
-		'../frontend/public/json/newly_railway.json.br',
-	]);
+	assert.equal(result.files.length, 8);
+	assert.equal(result.files.includes('../frontend/public/json/newly.json'), true);
+	assert.equal(result.files.includes('../frontend/public/json/newly.json.br'), true);
+	assert.equal(result.files.includes('../frontend/public/json/newly_web.json'), true);
+	assert.equal(result.files.includes('../frontend/public/json/newly_web.json.br'), true);
+	assert.equal(result.files.includes('../frontend/public/json/newly_kumeta.json'), true);
+	assert.equal(result.files.includes('../frontend/public/json/newly_kumeta.json.br'), true);
+	assert.equal(result.files.includes('../frontend/public/json/newly_railway.json'), true);
+	assert.equal(result.files.includes('../frontend/public/json/newly_railway.json.br'), true);
 });
