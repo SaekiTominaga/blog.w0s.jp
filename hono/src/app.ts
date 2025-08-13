25a27ca6a0d2a1b4d2f06e3329484745c0f2eaa8
SaekiTominaga <kcsu3470+github@gmail.com>
2025-08-13 11:27:45 +0900

    dotenv を node:process に置き換え

--- hono/src/app.ts
+++ hono/src/app.ts
@@ -1,7 +1,7 @@
 import crypto from 'node:crypto';
 import fs from 'node:fs';
 import path from 'node:path';
-import dotenv from 'dotenv';
+import { loadEnvFile } from 'node:process';
 import { Hono } from 'hono';
 import { basicAuth } from 'hono/basic-auth';
 import { compress } from 'hono/compress';
@@ -20,9 +20,7 @@ import { getAuth } from './util/auth.js';
 import { env } from './util/env.js';
 import { csp as cspHeader, reportingEndpoints as reportingEndpointsHeader } from './util/httpHeader.js';
 
-dotenv.config({
-	path: process.env['NODE_ENV'] === 'production' ? '../.env.production' : '../.env.development',
-});
+loadEnvFile(process.env['NODE_ENV'] === 'production' ? '../.env.production' : '../.env.development');
 
 /* Logger 設定 */
 Log4js.configure(env('LOGGER'));
