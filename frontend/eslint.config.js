66f94fa0f2d34f3b6d4a07432a2514de37264233
SaekiTominaga <kcsu3470+github@gmail.com>
2025-09-18 15:13:28 +0900

    型指定変更

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,7 +2,7 @@
 
 import w0sConfig from '@w0s/eslint-config';
 
-/** @type {import("@typescript-eslint/utils/ts-eslint").FlatConfig.ConfigArray} */
+/** @type {import("eslint").Linter.Config[]} */
 export default [
 	...w0sConfig,
 	{
