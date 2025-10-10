c2cb661d770e9f80e432a5fc7efbc2b95d14ab43
SaekiTominaga <kcsu3470+github@gmail.com>
2025-10-10 21:08:07 +0900

    test 時に logger が機能していなかったバグを修正

--- hono/src/process/newlyJson.test.ts
+++ hono/src/process/newlyJson.test.ts
@@ -1,6 +1,10 @@
 import { strict as assert } from 'node:assert';
 import { test } from 'node:test';
+import Log4js from 'log4js';
+import { env } from '@w0s/env-value-type';
 import create from './newlyJson.ts';
+
+Log4js.configure(env('LOGGER'));
 
 await test('create', async () => {
 	const result = await create();
--- hono/src/process/snsMastodon.test.ts
+++ hono/src/process/snsMastodon.test.ts
@@ -1,6 +1,10 @@
 import { strict as assert } from 'node:assert';
 import { test } from 'node:test';
+import Log4js from 'log4js';
+import { env } from '@w0s/env-value-type';
 import post from './snsMastodon.ts';
+
+Log4js.configure(env('LOGGER'));
 
 await test('minimum properties', async () => {
 	const result = await post({
