f708c3ba37dc4c9b134e6c66a8e2a52f2fc8b120
SaekiTominaga <kcsu3470+github@gmail.com>
2025-11-09 01:06:49 +0900

    lint設定更新

--- remark/src/Markdown.ts
+++ remark/src/Markdown.ts
@@ -4,6 +4,7 @@ import rehypeStringify from 'rehype-stringify';
 import remarkParse from 'remark-parse';
 import remarkLintBlockquoteIndentation from 'remark-lint-blockquote-indentation';
 import remarkLintCodeBlockStyle from 'remark-lint-code-block-style';
+import remarkLintDefinitionCase from 'remark-lint-definition-case';
 import remarkLintEmphasisMarker from 'remark-lint-emphasis-marker';
 import remarkLintFencedCodeFlag from 'remark-lint-fenced-code-flag';
 import remarkLintFencedCodeMarker from 'remark-lint-fenced-code-marker';
@@ -14,11 +15,13 @@ import remarkLintListItemBulletIndent from 'remark-lint-list-item-bullet-indent'
 import remarkLintListItemContentIndent from 'remark-lint-list-item-content-indent';
 import remarkLintListItemIndent from 'remark-lint-list-item-indent';
 import remarkLintNoBlockquoteWithoutMarker from 'remark-lint-no-blockquote-without-marker';
+import remarkLintNoDuplicateDefinitions from 'remark-lint-no-duplicate-definitions';
 import remarkLintNoHeadingContentIndent from 'remark-lint-no-heading-content-indent';
-import remarkLintNoInlinePadding from 'remark-lint-no-inline-padding';
 import remarkLintNoMissingBlankLines from 'remark-lint-no-missing-blank-lines';
 import remarkLintNoShortcutReferenceLink from 'remark-lint-no-shortcut-reference-link';
 import remarkLintNoTableIndentation from 'remark-lint-no-table-indentation';
+import remarkLintNoUndefinedReferences from 'remark-lint-no-undefined-references';
+import remarkLintNoUnusedDefinitions from 'remark-lint-no-unused-definitions';
 import remarkLintOrderedListMarkerStyle from 'remark-lint-ordered-list-marker-style';
 import remarkLintOrderedListMarkerValue from 'remark-lint-ordered-list-marker-value';
 import remarkLintStrongMarker from 'remark-lint-strong-marker';
@@ -29,7 +32,7 @@ import remarkRehype from 'remark-rehype';
 import { type Processor, unified } from 'unified';
 import type { VFile } from 'unified-lint-rule/lib/index.ts';
 import http from 'highlight.js/lib/languages/http';
-import configRemark from './config.ts';
+import config from './config.ts';
 import footnoteHast from './hast/footnote.ts';
 import remarkLintHeadingDepthLimit from './lint/headingDepthLimit.ts';
 import remarkLintNoEmptySections from './lint/noEmptySection.ts';
@@ -77,42 +80,70 @@ export default class Markdown {
 		const processor = unified();
 
 		if (lint) {
-			processor.use(remarkLintNoTypes, ['break', 'definition', 'image', 'thematicBreak']); // 使用禁止要素
-			processor.use(remarkLintNoMissingBlankLines); // ブロック間の空行必須
-			processor.use(remarkLintNoHeadingContentIndent); // [recommended] 見出し記号と内容の間のスペースは1つのみ
-			processor.use(remarkLintFirstHeadingLevel, 1); // 最初の見出しは 1
-			processor.use(remarkLintHeadingIncrement); // [markdown-style-guide] 見出しの数字飛ばし
-			processor.use(remarkLintHeadingDepthLimit, configRemark.headingDepthLimit); // 見出しレベルの最大値
-			processor.use(remarkLintHeadingStyle, 'atx'); // [markdown-style-guide] 見出し構文
-			processor.use(remarkLintNoEmptySections); // セクション内にコンテンツが存在すること
-			processor.use(remarkLintListItemBulletIndent); // [recommended] リスト項目のインデント禁止
-			processor.use(remarkLintListItemContentIndent); // [markdown-style-guide] リスト項目のインデント統一
-			processor.use(remarkLintListItemIndent, 'space'); // [markdown-style-guide][recommended] リスト項目のビュレットと内容の間をスペースに統一
-			processor.use(remarkLintUnorderedListMarkerStyle, '-'); // [markdown-style-guide] <ol> のマーカー表記
-			processor.use(remarkLintOrderedListMarkerStyle, '.'); // [markdown-style-guide][recommended] <ol> のマーカー表記
-			processor.use(remarkLintOrderedListMarkerValue, 'one'); // [markdown-style-guide] <ol> のマーカー表記
-			processor.use(remarkLintNoInlinePadding); // [markdown-style-guide][recommended] リンク文字列にスペースを含めることを禁止（<em> など他のフレージングコンテンツには効果なし）
-			processor.use(remarkLintNoLooseList); // Loose list 禁止
-			processor.use(remarkLintBlockquoteIndentation, 2); // [markdown-style-guide] <blockquote> のインデント（ > と内容の間のスペース、なお > 自体の文字数を含む）
-			processor.use(remarkLintNoBlockquoteWithoutMarker); // [markdown-style-guide][recommended] <blockquote> 内において > で始まらない行を禁止
-			processor.use(remarkLintCodeBlockStyle, 'fenced'); // [markdown-style-guide] <pre><code> の言語
+			/* remark-lint: [style-guide][recommended] 設定コメントは使用しないので不要 */
+			processor.use(remarkLintBlockquoteIndentation, 2); // [style-guide] <blockquote> のインデント（ > と内容の間のスペース、なお > 自体の文字数を含む）
+			processor.use(remarkLintCodeBlockStyle, 'fenced'); // [style-guide] <pre><code> 構文
+			processor.use(remarkLintDefinitionCase); // [style-guide] 定義ラベルの冒頭は小文字
+			/* remark-lint-definition-spacing: [style-guide] definition は使用禁止設定にしているので不要 */
+			processor.use(remarkLintEmphasisMarker, '*'); // [style-guide] <em> 構文
 			processor.use(remarkLintFencedCodeFlag, {
 				allowEmpty: true,
-				flags: configRemark.codeLanguages,
-			}); // [markdown-style-guide] <pre><code> 構文
-			processor.use(remarkLintFencedCodeMarker, '`'); // [markdown-style-guide] <pre><code> 構文
-			processor.use(remarkLintNoTableIndentation); // [markdown-style-guide] <table> のインデント禁止
-			processor.use(remarkLintTablePipes); // [markdown-style-guide] <table> の最初と末尾の | 必須
-			processor.use(remarkLintTableCellPadding, 'padded'); // [markdown-style-guide] <th>, <td> の | と内容の間のスペース
+				flags: config.codeLanguages,
+			}); // [style-guide] <pre><code> の言語
+			processor.use(remarkLintFencedCodeMarker, '`'); // [style-guide] <pre><code> 構文
+			/* remark-lint-file-extension: [style-guide] ファイルからの読み込みは使用していないので不要 */
+			/* remark-lint-final-definition: [style-guide] 要検討 */
+			/* remark-lint-final-newline: [recommended] 最終行の空白はむしろ JavaScript で除去しているので競合してしまう */
+			processor.use(remarkLintFirstHeadingLevel, 1); // 最初の見出しは 1
+			/* remark-lint-hard-break-spaces: [style-guide][recommended] break は使用禁止設定にしているので不要 */
+			processor.use(remarkLintHeadingDepthLimit, config.headingDepthLimit); // 見出しレベルの最大値
+			processor.use(remarkLintHeadingIncrement); // [style-guide] 見出しの数字飛ばし
+			processor.use(remarkLintHeadingStyle, 'atx'); // [style-guide] 見出し構文
+			/* remark-lint-link-title-style: [style-guide] リンクタイトルは使用禁止設定にしているので不要 */
+			processor.use(remarkLintListItemBulletIndent); // [recommended] リスト項目のインデント禁止
+			processor.use(remarkLintListItemContentIndent); // [style-guide] リスト項目のインデント統一
+			processor.use(remarkLintListItemIndent, 'one'); // [style-guide][recommended] リスト項目のビュレットと内容の間をスペースに統一
+			/* remark-lint-list-item-spacing: [style-guide] 要検討 */
+			/* remark-lint-maximum-heading-length: [style-guide] 不要 */
+			/* remark-lint-maximum-line-length: [style-guide] 不要 */
+			processor.use(remarkLintNoBlockquoteWithoutMarker); // [style-guide][recommended] <blockquote> 内において > で始まらない行を禁止
+			/* remark-lint-no-consecutive-blank-lines: [style-guide] <dl> での空行が誤検知されてしまう */
+			processor.use(remarkLintNoDuplicateDefinitions); // [recommended] 参照先が重複していないこと
+			/* remark-lint-no-duplicate-headings: [style-guide] 空見出しが引っ掛かってしまう */
+			/* no-emphasis-as-heading: [style-guide] 不要 */
+			processor.use(remarkLintNoEmptySections); // セクション内にコンテンツが存在すること
+			/* remark-lint-no-file-name-articles: [style-guide] ファイルからの読み込みは使用していないので不要 */
+			/* remark-lint-no-file-name-consecutive-dashes: [style-guide] ファイルからの読み込みは使用していないので不要 */
+			/* remark-lint-no-file-name-irregular-characters: [style-guide] ファイルからの読み込みは使用していないので不要 */
+			/* remark-lint-no-file-name-mixed-case: [style-guide] ファイルからの読み込みは使用していないので不要 */
+			/* remark-lint-no-file-name-outer-dashes: [style-guide] ファイルからの読み込みは使用していないので不要 */
+			processor.use(remarkLintNoHeadingContentIndent); // [recommended] 見出し記号と内容の間のスペースは1つのみ
+			/* remark-lint-no-heading-punctuation: [style-guide] 不要 */
+			/* remark-lint-no-literal-urls: [style-guide][recommended] 本文中であえてリンクにしない URL を記述することがあるので不要 */
 			processor.use(remarkLintNoLinkTitle); // リンクの title 禁止
-			processor.use(remarkLintNoShortcutReferenceLink); // [markdown-style-guide][recommended] 参照リンクでは末尾の [] が必須
-			processor.use(remarkLintEmphasisMarker, '*'); // [markdown-style-guide] <em> 構文
-			processor.use(remarkLintStrongMarker, '*'); // [markdown-style-guide] <strong> 構文
+			processor.use(remarkLintNoLooseList); // Loose list 禁止
+			processor.use(remarkLintNoMissingBlankLines, { exceptTightLists: true }); // ブロック間の空行必須
+			/* remark-lint-no-multiple-toplevel-headings: [style-guide] # を見出しレベル2の扱いにしているので当然複数出現は許容する */
+			/* remark-lint-no-shell-dollars: [style-guide] 不要 */
+			/* remark-lint-no-shortcut-reference-image: [style-guide][recommended] image は使用禁止設定にしているので不要 */
+			processor.use(remarkLintNoShortcutReferenceLink); // [style-guide][recommended] 参照リンクでは末尾の [] が必須
+			processor.use(remarkLintNoTableIndentation); // [style-guide] <table> のインデント禁止
+			processor.use(remarkLintNoTypes, ['break', 'definition', 'image', 'thematicBreak']); // 使用禁止要素
+			processor.use(remarkLintNoUndefinedReferences); // [recommended] 参照先が存在すること
+			processor.use(remarkLintNoUnusedDefinitions); // [recommended] 参照元が存在すること
+			processor.use(remarkLintOrderedListMarkerStyle, '.'); // [style-guide][recommended] <ol> のマーカー表記
+			processor.use(remarkLintOrderedListMarkerValue, 'one'); // [style-guide] <ol> のマーカー表記
+			/* remark-lint-rule-style: [style-guide] thematicBreak は使用禁止設定にしているので不要 */
+			processor.use(remarkLintStrongMarker, '*'); // [style-guide] <strong> 構文
+			processor.use(remarkLintTableCellPadding, 'padded'); // [style-guide] <th>, <td> の | と内容の間のスペース
+			/* remark-lint-table-pipe-alignment: [style-guide] 要検討 */
+			processor.use(remarkLintTablePipes); // [style-guide] <table> の最初と末尾の | 必須
+			processor.use(remarkLintUnorderedListMarkerStyle, '-'); // [style-guide] <ol> のマーカー表記
 		}
 
 		processor.use(remarkParse); // Markdown → mdast
 
-		processor.use(headingToMdast, { maxDepth: configRemark.headingDepthLimit }); // toc 処理より前に実行する必要がある
+		processor.use(headingToMdast, { maxDepth: config.headingDepthLimit }); // toc 処理より前に実行する必要がある
 		processor.use(tocToMdast); // section 処理より前に実行する必要がある
 		processor.use(blankToMdast);
 		processor.use(blockquoteToMdast);
@@ -120,7 +151,7 @@ export default class Markdown {
 		processor.use(defListToMdast);
 		processor.use(embeddedToMdast);
 		processor.use(footnoteToMdast);
-		processor.use(sectionToMdast, { maxDepth: configRemark.headingDepthLimit });
+		processor.use(sectionToMdast, { maxDepth: config.headingDepthLimit });
 		processor.use(tableToMdast);
 
 		processor.use(remarkRehype, {
