d29dec1ed243cdcd297d86ccec122c43aee35a59
SaekiTominaga <kcsu3470+github@gmail.com>
2025-11-20 19:15:57 +0900

    watch モード対応

--- frontend/build/svg.ts
+++ frontend/build/svg.ts
@@ -1,8 +1,9 @@
 import fs from 'node:fs';
 import path from 'node:path';
 import { parseArgs } from 'node:util';
+import { watch as chokidarWatch } from 'chokidar';
 import slash from 'slash';
-import { loadConfig, optimize } from 'svgo';
+import { loadConfig as svgoLoadConfig, optimize as svgOptimize, type Output as SvgoOutput } from 'svgo';
 
 /**
  * SVG ビルド
@@ -23,6 +24,11 @@ const argsParsedValues = parseArgs({
 			type: 'string',
 			short: 'c',
 		},
+		watch: {
+			type: 'boolean',
+			short: 'w',
+			default: false,
+		},
 	},
 }).values;
 
@@ -35,28 +41,52 @@ if (argsParsedValues.outDir === undefined) {
 if (argsParsedValues.config === undefined) {
 	throw new Error('Argument `config` not specified');
 }
-const filesPath = slash(`${argsParsedValues.inDir}/**/*.svg`);
+const inDirectory = slash(argsParsedValues.inDir);
 const outDirectory = slash(argsParsedValues.outDir);
 const configFilePath = slash(argsParsedValues.config);
+const { watch } = argsParsedValues;
 
-const config = await loadConfig(configFilePath);
+const svgoConfig = await svgoLoadConfig(configFilePath);
 
-const files = await Array.fromAsync(fs.promises.glob(filesPath));
+const optimize = async (filePathTemp: string): Promise<void> => {
+	const filePath = slash(filePathTemp);
 
-await Promise.all(
-	files.map(async (filePath) => {
-		/* ファイル読み込み */
-		const fileData = (await fs.promises.readFile(filePath)).toString();
+	/* ファイル読み込み */
+	const fileData = (await fs.promises.readFile(filePath)).toString();
 
-		/* SVG 最適化 */
-		const optimized = optimize(fileData.replace(/<svg version="([0-9.]+)"/v, '<svg').replace(' id="レイヤー_1"', ''), config);
+	/* SVG 最適化 */
+	let optimized: SvgoOutput;
+	try {
+		optimized = svgOptimize(fileData.replace(/<svg version="([0-9.]+)"/v, '<svg').replace(' id="レイヤー_1"', ''), svgoConfig);
+	} catch (e) {
+		if (e instanceof Error && e.name === 'SvgoParserError') {
+			// @ts-expect-error: ts(2339)
+			console.warn('[Error]', e.reason, `<${filePath}>`);
+			return;
+		}
+		throw e;
+	}
 
-		// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-		const outFileParsed = path.parse(filePath.replace(new RegExp(`^${argsParsedValues.inDir}`, 'v'), outDirectory));
-		const outPath = `${outFileParsed.dir}/${outFileParsed.name}.svg`;
+	const outFileParsed = path.parse(filePath.replace(new RegExp(`^${inDirectory}`, 'v'), outDirectory));
+	const outExtension = '.svg';
+	const outPath = `${outFileParsed.dir}/${outFileParsed.name}${outExtension}`;
 
-		/* 出力 */
-		await fs.promises.writeFile(outPath, optimized.data);
-		console.info(`SVG file optimized: ${outPath}`);
-	}),
-);
+	/* 出力 */
+	await fs.promises.mkdir(path.dirname(outPath), { recursive: true });
+	await fs.promises.writeFile(outPath, optimized.data);
+	console.info(`SVG file optimized: ${outPath}`);
+};
+
+const targetFiles = await Array.fromAsync(fs.promises.glob(`${inDirectory}/**/*.svg`));
+if (watch) {
+	chokidarWatch(targetFiles).on('change', (filePath) => {
+		// eslint-disable-next-line @typescript-eslint/no-floating-promises
+		optimize(filePath);
+	});
+} else {
+	await Promise.all(
+		targetFiles.map(async (filePath) => {
+			await optimize(filePath);
+		}),
+	);
+}
