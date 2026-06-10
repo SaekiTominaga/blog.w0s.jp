fad536cec9c2b733a0b617c9be0ecdc40fe33fe1
SaekiTominaga <kcsu3470+github@gmail.com>
2026-06-10 18:49:23 +0900

    キャッシュクリア機能に text 型でレスポンスする機能を追加

--- hono/src/controller/clear.test.ts
+++ hono/src/controller/clear.test.ts
@@ -33,6 +33,51 @@ await test('no auth', async () => {
 	}
 });
 
+await test('validator', async (t) => {
+	await t.test('response', async (t2) => {
+		await t2.test('array', async () => {
+			const formData = new FormData();
+			formData.append('response', 'foo1');
+			formData.append('response', 'foo2');
+
+			const res = await app.request('/api/clear', {
+				method: 'post',
+				headers: { Authorization: authorization },
+				body: formData,
+			});
+
+			assert.equal(res.status, 400);
+
+			const json = (await res.json()) as Post;
+
+			assert.equal('error' in json, true);
+			if ('error' in json) {
+				assert.equal(json.error.message, 'The `response` parameter can only be singular');
+			}
+		});
+
+		await t2.test('invalid string', async () => {
+			const formData = new FormData();
+			formData.append('response', 'foo');
+
+			const res = await app.request('/api/clear', {
+				method: 'post',
+				headers: { Authorization: authorization },
+				body: formData,
+			});
+
+			assert.equal(res.status, 400);
+
+			const json = (await res.json()) as Post;
+
+			assert.equal('error' in json, true);
+			if ('error' in json) {
+				assert.equal(json.error.message, 'The `response` parameter is invalid');
+			}
+		});
+	});
+});
+
 await test('no error', async () => {
 	const dao = new PostDao(`${env('ROOT')}/${env('SQLITE_DIR')}/${env('SQLITE_BLOG')}`);
 	const lastModifiledBefore = await dao.getLastModified();
@@ -46,15 +91,42 @@ await test('no error', async () => {
 
 	assert.equal(res.status, 200);
 	assert.equal(res.headers.get('Content-Type'), 'application/json');
-
-	const json = (await res.json()) as Post;
-
-	assert.equal(Array.isArray(json), true);
-	if (Array.isArray(json)) {
-		assert.equal(
-			json.every((result) => result.success),
-			true,
-		);
-	}
 	assert.equal(lastModifiledBefore < lastModifiledAfter, true);
+});
+
+await test('response', async (t) => {
+	await t.test('JSON', async () => {
+		const res = await app.request('/api/clear', {
+			method: 'post',
+			headers: { Authorization: authorization },
+		});
+
+		const json = (await res.json()) as Post;
+
+		assert.equal(Array.isArray(json), true);
+		if (Array.isArray(json)) {
+			assert.equal(
+				json.every((result) => result.success),
+				true,
+			);
+		}
+	});
+
+	await t.test('Text', async () => {
+		const formData = new FormData();
+		formData.append('response', 'text');
+
+		const res = await app.request('/api/clear', {
+			method: 'post',
+			headers: { Authorization: authorization },
+			body: formData,
+		});
+
+		const text = await res.text();
+
+		assert.match(
+			text,
+			/^✅ DB 最終更新日時の記録に成功 <[0-9]{2}:[0-9]{2}:[0-9]{2}>\n✅ フィード生成に成功（[0-9]+ファイル）\n✅ サイトマップ生成に成功（[0-9]+ファイル）\n✅ 新着 JSON ファイル生成に成功（[0-9]+ファイル）$/v,
+		);
+	});
 });
